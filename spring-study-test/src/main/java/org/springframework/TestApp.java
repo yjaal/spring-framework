904af4508b1d53ae7688706264aefe6af94423ba
yjaal <yjjal1314@gmail.com>
2020-05-05 15:25:19 +0800

    编译提交

--- spring-study-test/src/main/java/org/springframework/TestApp.java
+++ spring-study-test/src/main/java/org/springframework/TestApp.java
@@ -1,5 +1,15 @@
 package org.springframework;
 
+import java.util.Arrays;
+import org.springframework.conf.Config;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class TestApp {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+		String[] beanNames = context.getBeanDefinitionNames();
+		Arrays.asList(beanNames).forEach(System.out::println);
+	}
 
 }
--- spring-study-test/src/main/java/org/springframework/conf/Config.java
+++ spring-study-test/src/main/java/org/springframework/conf/Config.java
@@ -1,4 +1,4 @@
-package org.springframework;
+package org.springframework.conf;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
